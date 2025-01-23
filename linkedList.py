d5bba40130cb9d92671dec64485828ae5bb13f3c
thiagovsorio <thiagovsorio@gmail.com>
2025-01-23 00:15:33 -0300

    chore: reorganizing project directories

--- linkedList.py
+++ linkedList.py
@@ -23,6 +23,12 @@ class LinkedList:
         else:
             self.tail = self.head = node
 
+    def deque(self):
+        if self.head:
+            current_head = self.head.data
+            self.head = self.head.next
+            return current_head
+
     def print_list(self):
         current_node = self.head
         while current_node:
@@ -34,6 +40,7 @@ linked_list = LinkedList()
 linked_list.insert("A")
 linked_list.insert("B")
 linked_list.insert("C")
+linked_list.deque()
 
 print("Linked List:")
 linked_list.print_list()
